a13969fc6b9da78f898a797ac9d2bdad9bf15c72
imrudakov <imrudakov@gmail.com>
2021-01-25 20:08:42 +1000

    first commit

--- api/config/database.php
+++ api/config/database.php
@@ -5,7 +5,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "api_db";
     private $username = "root";
-    private $password = "";
+    private $password = "root";
     public $conn;
 
     // получаем соединение с БД
